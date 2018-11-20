64747c246e7648005573f08781601feaaee6b107
qqgg231 <qqgg231@users.noreply.github.com>
2018-11-20 15:20:52 +0100

    added version
    oznaczyłem jako wersję 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='allegroapi',
-    # version='0.0.1',       # currently no version
+    version='0.0.1',    
     description='Simple wrapper (client) for Allegro.pl REST API written in Python (using requests)',
     url='https://github.com/chawel/python-allegro.git',
     license=license,
