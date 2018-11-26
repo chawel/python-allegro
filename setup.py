ee97fc4246120700561b0a42c529cd437715d35b
qqgg231 <qqgg231@users.noreply.github.com>
2018-11-26 22:27:20 +0100

    ability to install via `pip install -e .` (#4)
    * ability to install via `pip install -e .` + fixed .gitignore
    
    * fixed pip package name in setup.py
    
    * added version
    
    oznaczyłem jako wersję 0.0.1

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='allegroapi',
+    version='0.0.1',    
+    description='Simple wrapper (client) for Allegro.pl REST API written in Python (using requests)',
+    url='https://github.com/chawel/python-allegro.git',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+)
