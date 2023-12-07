85bca2c49d7a51afa88e968391ea3ea862ceb048
Put3k <fightepucio@gmail.com>
2023-12-07 14:19:05 +0100

    Make BaseApi _build_path method accept **kwargs to use them as parameters in path

--- allegroapi/base.py
+++ allegroapi/base.py
@@ -22,11 +22,17 @@ class BaseApi(object):
         self._a_client = a_client
         self.endpoint = ''
 
-    def _build_path(self, *args):
+    def _build_path(self, *args, **kwargs):
         """
         Build path with endpoint and args
 
         :param args: Path elements in the endpoint URL
         :type args: :py:class:`unicode`
         """
-        return '/'.join(chain((self.endpoint,), map(str, args)))
+        path = '/'.join(chain((self.endpoint,), map(str, args)))
+        if kwargs:
+            params_path = "&".join(f"{key}={value}" for key, value in kwargs.items())
+            path = path + "?" + params_path
+            print(path)
+
+        return path
